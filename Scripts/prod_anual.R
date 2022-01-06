26ad2195856f8fe86f06ef45b39395a8f693614c
momaju <momaju@gmail.com>
2022-01-06 12:02:24 -0300

    palete viridis

--- Scripts/prod_anual.R
+++ Scripts/prod_anual.R
@@ -25,3 +25,21 @@ Biom %>%
   theme(plot.caption = element_text(size = 9, color = "grey60")) +
   geom_text(aes(label = format(biom_real, big.mark = ".", decimal.mark = ",")), vjust=1.6, color="white", size=4.0)
 
+
+# Trocando a a pllete de cores 
+
+Biom %>% 
+  mutate(ano_desp = factor(year(data_desp))) %>% #extrai o ano da data de despesca
+  group_by(ano_desp) %>%
+  summarize(biom_real = round(sum(biom_real),2)) %>% 
+  ggplot(aes(x=ano_desp, y=biom_real, fill = ano_desp)) +
+  geom_bar(stat="identity", width = 0.5,show.legend = FALSE) +
+  labs(title = "Produção Anual (kg)",
+       y = "Kg Produzidos",
+       x = "Ano",
+       caption = "Fonte: Azul Marinho Aquicultura")+
+  theme_minimal() +
+  theme(plot.caption = element_text(size = 9, color = "grey60")) +
+  geom_text(aes(label = format(biom_real, big.mark = ".", decimal.mark = ",")), vjust=1.6, color="black", size=4.0) +
+  scale_fill_viridis_d(option = "viridis")
+
