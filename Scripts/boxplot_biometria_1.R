51d572d835987ffc5fa23e03bcf2d12c83d712e2
momaju <momaju@gmail.com>
2022-04-20 08:38:34 -0300

    mutate viveiro variable

--- Scripts/boxplot_biometria_1.R
+++ Scripts/boxplot_biometria_1.R
@@ -7,10 +7,12 @@ library(scales)
 Biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 
 Biom %>% 
-  select(viveiro, biometria_1) %>% 
-  ggplot(aes(factor(viveiro), biometria_1))+
+  mutate(viveiro = factor(viveiro),
+         mean_b1 = mean(biometria_1)) %>%
+  select(viveiro, biometria_1, mean_b1) %>%
+  ggplot(aes(viveiro, biometria_1))+
   geom_boxplot()+
-  geom_hline(yintercept = mean(Biom$biometria_1), colour = "red") + 
+  geom_hline(yintercept = mean(Biom$biometria_1), colour = "#4C00FF") + 
   labs(title = "Primeira Biometria",
   subtitle =  "Peso em g",
   y = "Peso na Primeira Biometria",
