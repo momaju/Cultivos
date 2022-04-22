64dade360f4008f1746b433e684fc85f09a13e45
momaju <momaju@gmail.com>
2022-04-22 09:04:18 -0300

    Modificado o select de variáveis

--- Scripts/boxplot_biometria_1.R
+++ Scripts/boxplot_biometria_1.R
@@ -9,7 +9,7 @@ Biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 Biom %>% 
   mutate(viveiro = factor(viveiro),
          mean_b1 = mean(biometria_1)) %>%
-  select(viveiro, biometria_1, mean_b1) %>%
+  select(viveiro, biometria_1) %>%
   ggplot(aes(viveiro, biometria_1))+
   geom_boxplot()+
   geom_hline(yintercept = mean(Biom$biometria_1), colour = "#4C00FF") + 
