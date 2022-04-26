3d24d5518f7d12a12cee744c439c6e308dfd5bfa
momaju <momaju@gmail.com>
2022-04-26 18:27:07 -0300

    Change plot color. Add origin to plot

--- Scripts/Crescimento.R
+++ Scripts/Crescimento.R
@@ -34,8 +34,12 @@ summary(fit) # show results
 sp <- Biom %>% 
   drop_na(densidade, g_semana) %>% 
   ggplot(aes(x = densidade, y = g_semana)) + 
-  geom_point(size = 4, alpha = 0.5, color = "blue") + 
-  geom_smooth(method = lm, se = F) +
+  geom_point(size = 4, alpha = 0.5, color = "#eca11a") + 
+  geom_smooth(method = lm, 
+              se = F,
+              color = "#c9543c") +
+  xlim(0,30) +
+  ylim(0,2.5)+
   labs(title = "Crescimento Médio Semanal em Função de Densidade de Estocagem",
        y = "Peso (g)",
        x = "Densidade (ind/mq)",
@@ -44,17 +48,17 @@ sp <- Biom %>%
   annotate("text",x=c(17,17,17),y=c(2.0, 1.9, 1.8), 
            label=c("p-value:  1.582e-07",
                    "R-squared:  0.2332",
-                   "g = 1.52 * -0,04dens"), 
+                   "g = 1.52 -0,04 * densidade"), 
            size = 6, 
            hjust = 0,
-           color = "blue") +
+           color = "#eca11a") +
         theme(plot.caption = element_text(size = 9, color = "grey60"),
         axis.text.y = element_text(size = 12, color = "black"),
         axis.text.x = element_text(size = 12, color = "black"),
         axis.title.y = element_text(size = 15, color = "black"),
         axis.title.x = element_text(size = 15, color = "black"))
 
-sp
+sp 
 
 
 
