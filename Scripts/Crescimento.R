3c2e3b07f73988c542203457e2bcf76aa0c4cd80
momaju <momaju@gmail.com>
2022-05-01 07:00:30 -0300

    Add glimpse and View and some code cleaning

--- Scripts/Crescimento.R
+++ Scripts/Crescimento.R
@@ -15,13 +15,13 @@ biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 head(biom)
 summary(biom)
 str(biom)
-
-
+glimpse(biom)
+View(biom)
 
 # Regressão ---------------------------------------------------------------
 
 
-fit <- biom %>% 
+fit <- biom %>%
   lm(g_semana ~ densidade, data = .)
 fit
 summary(fit) # show results
@@ -31,25 +31,25 @@ summary(fit) # show results
 # Gráfico sem agrupar por viveiro -----------------------------------------
 
 
-sp <- biom %>% 
-  drop_na(densidade, g_semana) %>% 
-  ggplot(aes(x = densidade, y = g_semana)) + 
-  geom_point(size = 4, alpha = 0.5, color = "#eca11a") + 
-  geom_smooth(method = lm, 
+sp <- biom %>%
+  drop_na(densidade, g_semana) %>%
+  ggplot(aes(x = densidade, y = g_semana)) +
+  geom_point(size = 4, alpha = 0.5, color = "#eca11a") +
+  geom_smooth(method = lm,
               se = F,
               color = "#c9543c") +
-  xlim(0,30) +
-  ylim(0,2.5)+
+  xlim(0, 30) +
+  ylim(0, 2.5) +
   labs(title = "Crescimento Médio Semanal em Função de Densidade de Estocagem",
        y = "Peso (g)",
        x = "Densidade (ind/mq)",
-       caption = "Fonte: Azul Marinho Aquicultura")+
+       caption = "Fonte: Azul Marinho Aquicultura") +
   theme_minimal() +
-  annotate("text",x=c(17,17,17),y=c(2.0, 1.9, 1.8), 
-           label=c("p-value:  1.582e-07",
+  annotate("text", x = c(17, 17, 17), y = c(2.0, 1.9, 1.8),
+           label = c("p-value:  1.582e-07",
                    "R-squared:  0.2332",
-                   "g = 1.52 -0,04 * densidade"), 
-           size = 6, 
+                   "g = 1.52 -0,04 * densidade"),
+           size = 6,
            hjust = 0,
            color = "#eca11a") +
         theme(plot.caption = element_text(size = 9, color = "grey60"),
@@ -58,8 +58,4 @@ sp <- biom %>%
         axis.title.y = element_text(size = 15, color = "black"),
         axis.title.x = element_text(size = 15, color = "black"))
 
-sp 
-
-
-
-
+sp
