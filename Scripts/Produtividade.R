314d07c3befb1107426e603612ac431bbb58716d
momaju <momaju@gmail.com>
2022-05-01 14:44:11 -0300

    code updates

--- Scripts/Produtividade.R
+++ Scripts/Produtividade.R
@@ -39,63 +39,47 @@ produtividade_ciclo %>%
          y = "Produtividade (kg/ha)") +
   theme_minimal() +
   theme(plot.caption = element_text(size = 8, color = "grey60", hjust = 1))
-  
 
 
 
 
-# Azul Marinho: Produtividade em kg/ha vs Densidade do povoamento em numero de camarões por metro quadrado. Os dados referem-se aos 10 ciclos de cultivo (2015 ~ 2017).
-
-biomassa <- read.csv(file = "Cultivo - biomassa.csv", header = T)
-
-head(Biomassa)
-
-Biomassa$viveiro = factor(Biomassa$viveiro, levels = c(1,2,3,4), labels = c("V1","V2","V3","V4"))
-
-Biomassa$ciclo = factor(Biomassa$ciclo,levels = c(1,2,3,4,5,6,7,8,9,10), labels = c("C1","C2","C3","C4","C5","C6","C7","C8","C9","C10"))
-
-Biomassa$ano = factor(Biomassa$ano)
-Biomassa$bionutri = factor(Biomassa$bionutri)
-
-str(Biomassa)
-
-
-
-library(ggplot2)
-library(ggthemes)
-
-a <- ggplot(data = biom, aes(x = densidade, y = produtividade)) + geom_point(shape = 1)
-
-a <- a + geom_smooth(method = lm, se = FALSE)
-a
-# Títulos
-
-a <- a + xlab("Densidade") + ylab("Produtividade kg/ha") + ggtitle("Azul Marinho\nProdutividade vs Densidade") 
-  #caption("2015-2017")
+a <- biom %>%
+ggplot(aes(x = densidade, y = produtividade)) +
+geom_point() +
+geom_smooth(method = lm, se = FALSE) +
+labs(title = "Produtividade em Função da Densidade de Povoamento",
+         subtitle = "",
+         caption = "Fonte: Azul Marinho Aquicultura",
+         x = "Povoamento\n(Cmarões/mq.)",
+         y = "Produtividade (kg/ha)") +
+  theme_minimal() +
+  theme(plot.caption = element_text(size = 8, color = "grey60", hjust = 1),
+        axis.text.y = element_text(size = 12, color = "black"),
+        axis.text.x = element_text(size = 12, color = "black"),
+        axis.title.y = element_text(size = 15, color = "black"),
+        axis.title.x = element_text(size = 15, color = "black"),
+        plot.margin = margin(25, 25, 25, 25)) +
+scale_x_continuous(expand = c(0, 0), limits = c(0, 30))
 a
 
-# Regressão
-fit <- lm(produtividade ~ densidade, data = biom)
+# Regressão--------------------------------------------
+
+fit <- biom %>%
+lm(produtividade ~ densidade, data = .)
 summary(fit)
 
-# Inserting the equation
+# Inserting the equation---------------------------------
 
-equation = function(fit) {
+equation <- function(fit) {
   lm_coef <- list(a = round(coef(fit)[1], digits = 2),
                   b = round(coef(fit)[2], digits = 2),
                   r2 = round(summary(fit)$r.squared, digits = 2));
-  lm_eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,lm_coef)
-  as.character(as.expression(lm_eq));                 
+  lm_eq <- substitute(italic(y) == a + b %.% italic(x)
+  * ","~~italic(R)^2~"="~r2, lm_coef)
+  as.character(as.expression(lm_eq));
 }
 
-b <-  a + annotate("text", x = 20, y = 450.00, label = equation(fit), parse = TRUE, color = "Blue", size=6)
+b <-  a + annotate("text", x = 20, y = 450.00,
+label = equation(fit), parse = TRUE, color = "blue")
 b
 b + theme_economist()
-
-
-
-
-
-
-
-
