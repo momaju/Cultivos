861eb6ea74d9d55efb8737ced5bfcad39b0cad13
momaju <momaju@gmail.com>
2022-05-06 16:04:30 -0300

    Initial commit

---
+++ Scripts/Previsão_de_Despesca.r
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(googlesheets4)
+library(ggthemes)
+library(lubridate)
+library(scales)
+library(corrplot)
+library(RColorBrewer)
+
+biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
+
+biom_numeric <- biom %>%
+  na.omit() %>%
+  select_if(is.numeric)
+
+  View(biom_numeric)
+
+cor(biom_numeric[, 3:19]) %>%
+  corrplot()
+
+# Previsão de Despesca ---------------------------------------------------
+
+biom %>%
+lm(biom_real ~ biom_calc + pop + ddc +
+                 densidade + baixa_mil, data = .) %>%
+predict(data.frame(biom_calc = c(2510),
+                  pop = c(350000),
+                  ddc = c(60),
+                  densidade = c(11.99),
+                  baixa_mil = c(0.5))) %>%
+round(2)
+
+## Regressão para um único viveiro-------------------------------------------
+
+biom_v2 <- biom %>%
+  na.omit() %>%
+  filter(viveiro == 2)
+biom_v2
+
+fit_biom_v2 <- biom_v2 %>%
+lm(biom_real ~ biom_calc + pop + ddc +
+                    densidade + baixa_mil, data = .)
+summary(fit_biom_v2)
