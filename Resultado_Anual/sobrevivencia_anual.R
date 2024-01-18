c5f6db2504702559c845644aa04368084b1ab4ea
momaju <momaju@gmail.com>
2024-01-18 14:46:08 -0300

    Boxplot added

--- Resultado_Anual/sobrevivencia_anual.R
+++ Resultado_Anual/sobrevivencia_anual.R
@@ -11,8 +11,10 @@ biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 
 sobrevive_ano <- biom %>%
   mutate(ano_desp = factor(year(data_desp))) %>%
+  filter(ano_desp != 2024) %>% 
   group_by(ano_desp) %>%
   summarize(sobrevive = median(sobrevive))
+
 
 sobrevive_ano %>%
   ggplot(aes(ano_desp, sobrevive)) +
@@ -21,6 +23,7 @@ sobrevive_ano %>%
   geom_line(group = 1, color = "#8080c0", linewidth = 1) +
   expand_limits(y = 30) +
   labs(title = "Sobrevivência Anual (%)",
+       subtitle = "2015 a 2023",
        x = "Ano",
        y = "Percentual",
        caption = "Azul Marinho Aquicultura") +
@@ -32,8 +35,12 @@ sobrevive_ano %>%
         axis.title.y = element_text(size = 15,
                                     color = "#000080",
                                     margin = margin(t = 0, r = 20, b = 0, l = 0)),
-        axis.title.x = element_text(size = 15, color = "#000080"),
-        plot.title = element_text(size = 20, color = "#000080"),
+        axis.title.x = element_text(size = 15, 
+                                    color = "#000080"),
+        plot.title = element_text(size = 20, 
+                                  color = "#000080"),
+        plot.subtitle = element_text(size = 15,
+                                     color = "#000080" ),
         axis.line.y = element_line(color = "#000080"),
         axis.line.x = element_line(color = "#000080"),
         panel.grid.major = element_blank())
@@ -42,3 +49,56 @@ sobrevive_ano %>%
 
 logo <- image_read("G://My Drive//RWork//Projects//Azul Marinho//Cultivos//Images//azul_logo_transp.png")
 grid::grid.raster(logo, x = 0.92, y = 0.8, just = c('left', 'bottom'), width = unit(1.5, 'inches'))
+
+
+
+
+# Boxplot sobrevivëncia ---------------------------------------------------
+
+sobrevive_viveiro <- biom %>%
+  mutate(ano_desp = factor(year(data_desp)), viveiro = factor(viveiro)) %>%
+  filter(ano_desp != 2024) %>%
+  group_by(viveiro)
+  #summarize(sobrevive = median(sobrevive))
+
+sobrevive_viveiro %>% 
+  ggplot(aes(viveiro, sobrevive)) +
+  geom_boxplot(fill = "#8080c0",
+               col = "#000080",
+               outlier.color = "#8080c0",
+               outlier.size = 4,
+               linetype = 1) +
+  stat_summary(fun = mean, geom = "point", shape = 8, size = 3, color = "#c0c080", fill = "#c0c080") + 
+  coord_flip() +
+  labs(title = "Sobrevivência Anual por Viveiro (%)",
+       subtitle = "2015 a 2023",
+       x = "Viveiro",
+       y = "Percentual",
+       caption = "Azul Marinho Aquicultura") +
+  theme_minimal() +
+  theme(plot.caption = element_text(size = 9, 
+                                    color = "#8080c0"),
+        axis.text.y = element_text(size = 15, 
+                                   color = "#000080"),
+        axis.text.x = element_text(size = 15, 
+                                   color = "#000080"),
+        axis.title.y = element_text(size = 15,
+                                    color = "#000080",
+                                    margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = 15, 
+                                    color = "#000080"),
+        plot.title = element_text(size = 20, 
+                                  color = "#000080"),
+        plot.subtitle = element_text(size = 15,
+                                     color = "#000080" ),
+        #axis.line.y = element_line(color = "#000080"),
+        #axis.line.x = element_line(color = "#000080"),
+        panel.grid.major = element_blank())
+
+# Inserindo o logo
+
+logo <- image_read("G://My Drive//RWork//Projects//Azul Marinho//Cultivos//Images//azul_logo_transp.png")
+grid::grid.raster(logo, x = 0.9, y = 0.8, just = c('left', 'bottom'), width = unit(1.5, 'inches'))
+
+
+  
