ff2dda19115f472ade784b3d4484d8bfd250504a
momaju <momaju@gmail.com>
2024-01-20 15:16:13 -0300

    File update

--- Scripts/Feed_costs.R
+++ Scripts/Feed_costs.R
@@ -31,19 +31,30 @@ feed_costs_long <- tidyr::pivot_longer(feed_costs,
 
 p <- ggplot(feed_costs_long) + 
   geom_col(aes(factor(mes, levels = feed_costs$mes), 
-               valor, fill = ano), position = position_dodge(width = 0.9), na.rm = FALSE) + 
+               valor, fill = ano), 
+           position = position_dodge(width = 0.9), 
+           na.rm = FALSE) + 
   
   
   ## add a hidden set of points to make the legend circles easily
-  geom_point(aes(x = mes, y = -10, color = ano, fill = ano), size = 5, na.rm = TRUE) + 
+  geom_point(aes(x = mes, y = -10, color = ano, fill = ano), 
+             size = 5, 
+             na.rm = TRUE) + 
   
   
   ## add the percentages just above each bar
  # geom_text(aes(mes, valor + 2, label = paste0("R$",valor), group = ano),
 #          position = position_dodge(width = 0.9), size = 3, vjust = -1.3, na.rm = FALSE) +
   
-  geom_text(aes(mes, valor + 2, label = format( valor, big.mark = ".", decimal.mark = ","), group = ano),
-            position = position_dodge(width = 0.9), size = 3, vjust = -1, na.rm = FALSE) +
+  geom_text(aes(mes, 
+                valor + 2, 
+                label = format( valor, big.mark = ".",
+                                decimal.mark = ","), 
+                group = ano),
+            position = position_dodge(width = 0.9), 
+            size = 3, 
+            vjust = -1, 
+            na.rm = FALSE) +
             
   
    ## use similar colours to the original
@@ -75,6 +86,86 @@ p <- ggplot(feed_costs_long) +
 p
 
 
+# Template Azul Marinho ---------------------------------------------------
+
+p <- ggplot(feed_costs_long) + 
+  geom_col(aes(factor(mes, levels = feed_costs$mes), 
+               valor, fill = ano), 
+           position = position_dodge(width = 0.9), 
+           na.rm = FALSE) + 
+  
+  
+  ## add a hidden set of points to make the legend circles easily
+  geom_point(aes(x = mes, y = -10, color = ano, fill = ano), 
+             size = 5, 
+             na.rm = TRUE) + 
+
+
+geom_text(aes(mes, 
+              valor + 2, 
+              label = format( valor, big.mark = ".",
+                              decimal.mark = ","), 
+              group = ano),
+          position = position_dodge(width = 0.9), 
+          size = 3, 
+          vjust = -1, 
+          na.rm = FALSE) +
+  
+  
+  ## use similar colours to the original
+  scale_fill_manual(values = c(`2020` = "#D39200", `2021` = "darkcyan")) +
+  scale_color_manual(values = c(`2020` = "#D39200", `2021` = "darkcyan")) + 
+  
+  
+  ## hide the fill legend and make the color legend horizontal
+  guides(fill = "none", color = guide_legend(direction = "horizontal")) +
+  scale_y_continuous(labels = scales::comma_format(scale = 1, prefix = "R$"), 
+                     limits = c(0, 35000)) +
+  labs(title = "Custos Mensais com Ração", 
+       subtitle = "2020 vs 2021", 
+       caption = "Fonte: Azul Marinho Aquicultura",
+       x = "", y = "") +
+  theme_minimal() + 
+  #theme(axis.text = element_text(size = 10),
+  #      plot.title = element_text(size = 28, hjust= 0.5), 
+  #      plot.subtitle = element_text(size = 20, hjust = 0.5),
+  #      plot.caption = element_text(size = 7, color = "grey60"),
+  #      plot.background = element_rect(fill = "#f4f7fc", size = 0),
+  #      legend.title = element_blank(),
+  #      legend.text= element_text(size = 12),
+  #      panel.grid = element_blank(),
+        
+  theme(plot.caption = element_text(size = 9, 
+                                    color = "#8080c0"),
+        axis.text.y = element_text(size = 15, 
+                                   color = "#000080"),
+        axis.text.x = element_text(size = 15, 
+                                   color = "#000080"),
+        axis.title.y = element_text(size = 20,
+                                    color = "#000080",
+                                    margin = margin(t = 0, 
+                                                    r = 20, 
+                                                    b = 0, l = 0)),
+        axis.title.x = element_text(size = 20, 
+                                    color = "#000080"),
+        plot.title = element_text(size = 25, 
+                                  color = "#000080"),
+        plot.subtitle = element_text(size = 18, 
+                                     color = "#000080"),
+        #plot.subtitle = element_markdown(size = 15, color = "#000080"),
+        axis.line.y = element_line(color = "#000080"),
+        axis.line.x = element_line(color = "#000080"),
+        panel.grid.major = element_blank()) +
+#  geom_text(aes(label = format(produtividade_media, 
+#                               big.mark = ".", 
+#                               decimal.mark = ",")),
+          #  vjust = -0.5, 
+          #  color = "#000080", 
+          #  size = 4.0)  
+
+        ## move the color legend to an inset 
+        legend.position = c(0.9, 0.9)
+p
 
 
 
