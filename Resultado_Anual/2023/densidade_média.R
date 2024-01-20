3820e13ee0513ac1b6a1b68876fcc050a1dcc222
momaju <momaju@gmail.com>
2024-01-20 10:59:49 -0300

    minor code styling

--- Resultado_Anual/2023/densidade_média.R
+++ Resultado_Anual/2023/densidade_média.R
@@ -36,21 +36,31 @@ biom_ano%>%
   expand_limits(y = 25) +
   scale_x_continuous(breaks = biom_ano$ano_desp) +
   theme_minimal() +
-  theme(plot.caption = element_text(size = 9, color = "#8080c0"),
-        axis.text.y = element_text(size = 15, color = "#000080"),
-        axis.text.x = element_text(size = 15, color = "#000080"),
+  theme(plot.caption = element_text(size = 9, 
+                                    color = "#8080c0"),
+        axis.text.y = element_text(size = 15, 
+                                   color = "#000080"),
+        axis.text.x = element_text(size = 15, 
+                                   color = "#000080"),
         axis.title.y = element_text(size = 20,
                                     color = "#000080",
                                     margin = margin(t = 0, r = 20, b = 0, l = 0)),
-        axis.title.x = element_text(size = 20, color = "#000080"),
-        plot.title = element_text(size = 25, color = "#000080"),
-        plot.subtitle = element_text(size = 18, color = "#000080"),
+        axis.title.x = element_text(size = 20, 
+                                    color = "#000080"),
+        plot.title = element_text(size = 25, 
+                                  color = "#000080"),
+        plot.subtitle = element_text(size = 18, 
+                                     color = "#000080"),
         #plot.subtitle = element_markdown(size = 15, color = "#000080"),
         axis.line.y = element_line(color = "#000080"),
         axis.line.x = element_line(color = "#000080"),
         panel.grid.major = element_blank()) +
-  geom_text(aes(label = format(densidade_media, big.mark = ".", decimal.mark = ",")),
-            vjust = -0.5, color = "#000080", size = 4.0) 
+  geom_text(aes(label = format(densidade_media, 
+                               big.mark = ".", 
+                               decimal.mark = ",")),
+            vjust = -0.5, 
+            color = "#000080", 
+            size = 4.0) 
 
 
 
@@ -62,5 +72,8 @@ biom_ano%>%
 
 image_url <- "https://drive.google.com/uc?id=1SN4gu5VzJYlfacpgoVycXNI8JRuswynA"
 logo <- image_read(image_url)
-
-grid::grid.raster(logo, x = 0.9, y = 0.8, just = c('left', 'bottom'), width = unit(1.3, 'inches'))
+grid::grid.raster(logo, 
+                  x = 0.9, 
+                  y = 0.8, 
+                  just = c('left', 'bottom'), 
+                  width = unit(1.3, 'inches'))
