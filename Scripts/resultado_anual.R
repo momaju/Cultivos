5f7577dd5fab8dd356cd3c9e5d4d8eaadaf5df25
momaju <momaju@gmail.com>
2024-01-31 14:24:12 -0300

    tweaks

--- Scripts/resultado_anual.R
+++ Scripts/resultado_anual.R
@@ -15,7 +15,7 @@ biom_ano <- biom %>%
   summarize(biom_real = round(sum(biom_real), 2))
 
 biom_ano %>% 
-  ggplot(aes(x = ano_desp, y = biom_real,)) +
+  ggplot(aes(x = ano_desp, y = biom_real)) +
   geom_bar(stat = "identity", width = 0.8, 
            show.legend = FALSE,
            fill = "#2e98fe") +
@@ -45,8 +45,17 @@ biom_ano %>%
         panel.grid.major = element_blank()) +
   geom_text(aes(label = format(biom_real, big.mark = ".", decimal.mark = ",")),
             vjust = -0.5, color = "#000080", size = 4.0) 
-logo <- image_read("G:/My Drive/RWork/Projects/Azul Marinho/Cultivos/Images/logo_azul.png")
-grid::grid.raster(logo, x = 0.005, y = 0.91, just =c("left", "bottom"), width = unit(2.5, 'cm'))
+
+
+# Inserindo o Logo --------------------------------------------------------
+
+image_url <- "https://drive.google.com/uc?id=1SN4gu5VzJYlfacpgoVycXNI8JRuswynA"
+logo <- image_read(image_url)
+grid::grid.raster(logo, 
+                  x = 0.9, 
+                  y = 0.8, 
+                  just = c('left', 'bottom'), 
+                  width = unit(1.3, 'inches'))
 
 # Trocando a a palete de cores (Viridis) test
 
@@ -71,7 +80,10 @@ biom %>%
         axis.text.x = element_text(size = 15, color = "#000080"),
         axis.title.y = element_text(size = 20,
                                     color = "#000080",
-                                    margin = margin(t = 0, r = 20, b = 0, l = 0)),
+                                    margin = margin(t = 0, 
+                                                    r = 20, 
+                                                    b = 0, 
+                                                    l = 0)),
         axis.title.x = element_text(size = 20, color = "#000080"),
         plot.title = element_text(size = 34, color = "#000080"),
         plot.subtitle = element_text(size = 17, color = "#000080"),
@@ -184,16 +196,85 @@ biom_mes %>% ggplot(aes(x = mes, y = mean_kg,)) +
   geom_text(aes(label = format(mean_kg, 
                                big.mark = ".", 
                                decimal.mark = ",")),
-            vjust = -0.5, color = "#000080", size = 4.0)
-logo <- image_read("G:/My Drive/RWork/Projects/Azul Marinho/Cultivos/Images/logo_azul.png")
-#grid::grid.raster(logo, x = 0.1, y = 0.02, just = c('left', 'bottom'), width = unit(1.9, 'inches'))
+            vjust = -0.5, 
+            color = "#000080", 
+            size = 4.0)
 
+# Inserindo o Logo --------------------------------------------------------
 
-grid::grid.raster(logo, x = 0.001,
-                  y = 0.91, 
-                  just =c("left", "bottom"), 
-                  width = unit(2.4, 'cm'))
+image_url <- "https://drive.google.com/uc?id=1SN4gu5VzJYlfacpgoVycXNI8JRuswynA"
+logo <- image_read(image_url)
+grid::grid.raster(logo, 
+                  x = 0.9, 
+                  y = 0.8, 
+                  just = c('left', 'bottom'), 
+                  width = unit(1.3, 'inches'))
 
 biom_mes
+
+
+# Produção 2023 -----------------------------------------------------------
+
+biom_2023 <- biom %>%
+  mutate(ano_desp = factor(year(data_desp))) %>% 
+  filter(ano_desp == "2023") %>%
+  mutate(mes_desp = month(data_desp,
+                          label = TRUE, 
+                          abbr = TRUE)) %>% 
+  group_by(mes_desp) %>%
+  summarize(biom_real = round(sum(biom_real, na.rm = TRUE), 2))
+
+biom_2023
+
+biom_2023 %>% ggplot(aes(x = mes_desp, y = biom_real)) +
+  geom_bar(stat = "identity", width = 0.8, 
+           show.legend = FALSE,
+           fill = "#2e98fe") +
+  labs(title = "2023 - Produção Mensal",
+       #subtitle = "2023",
+       y = "Kg Produzidos",
+       x = "",
+       caption = "Azul Marinho Aquicultura") +
+  scale_y_continuous(
+    limits = (c(0, 5000)),
+    breaks = (seq(0, 5000, 500)),
+    labels = scales::label_number(big.mark = ".",
+                                  decimal.mark = ","),
+    expand = expansion(0)) + #faz as barras encostarem no eixo
+  #expand_limits(y = 4000) +
+  theme_minimal() +
+  theme(plot.caption = element_text(size = 9, color = "#8080c0"),
+        axis.text.y = element_text(size = 15, color = "#000080"),
+        axis.text.x = element_text(size = 15, color = "#000080"),
+        axis.title.y = element_text(size = 20,
+                                    color = "#000080",
+                                    margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = 20, color = "#000080"),
+        plot.title = element_text(size = 25, color = "#000080"),
+        plot.subtitle = element_text(size = 12, color = "#000080"),
+        axis.line.y = element_line(color = "#000080"),
+        axis.line.x = element_line(color = "#000080"),
+        panel.grid.major = element_blank(),
+        plot.margin = margin(25,25,25,30)) +
+  geom_text(aes(label = format(biom_real, 
+                               big.mark = ".", 
+                               decimal.mark = ",")),
+            vjust = -0.5, color = "#000080", size = 4.0)
+
+
+# Inserindo o Logo --------------------------------------------------------
+
+image_url <- "https://drive.google.com/uc?id=1SN4gu5VzJYlfacpgoVycXNI8JRuswynA"
+logo <- image_read(image_url)
+grid::grid.raster(logo, 
+                  x = 0.9, 
+                  y = 0.8, 
+                  just = c('left', 'bottom'), 
+                  width = unit(1.3, 'inches'))
+
+
+
+
+
 
 
