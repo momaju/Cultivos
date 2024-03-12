0b584f36a2c55bb0f5e21181f61d6d44f0245986
momaju <momaju@gmail.com>
2024-03-12 15:30:13 -0300

    File update

--- Despesca_predict.R
+++ Despesca_predict.R
@@ -18,9 +18,9 @@ biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 biom %>%
   lm(biom_real ~ biom_calc + pop + ddc +
        densidade + baixa_mil, data = .) %>%
-  predict(data.frame(biom_calc = c(2600,2007),
+  predict(data.frame(biom_calc = c(3330,1850),
                      pop = c(470000,350000),
-                     ddc = c(60,60),
+                     ddc = c(45,41),
                      densidade = c(12.05,11.99),
-                     baixa_mil = c(0.098,0.034))) %>%
+                     baixa_mil = c(0.03,0.02))) %>%
   round(2)
