a52b940610330cad0b036c50f7549b8bf7abf5d9
momaju <momaju@gmail.com>
2024-03-21 13:16:51 -0300

    File updtae

--- Viveiro_03/v3_resultados.R
+++ Viveiro_03/v3_resultados.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 library(googlesheets4)
-library(ggthemes)
+library(ggthemes) 
 library(lubridate)
 library(ggrepel)
 library(scales)
@@ -17,7 +17,7 @@ biom <- read_sheet("1KkLM7bz-Az-etHUeENou-BjX4mDUfJCccwcCIo0k0CU", 2)
 
 v3_resumo <- biom %>%
   filter(viveiro == 3) %>%
-  select(data_desp, lab, biom_real, densidade, sobrevive, g_semana, g_final, ddc, baixa_mil, tca) %>% 
+  select(ciclo, data_desp, lab, biom_real, densidade, sobrevive, g_semana, g_final, ddc, baixa_mil, tca) %>% 
   filter(densidade >= 12 & densidade <= 13)
 
 v3_resumo %>% 
